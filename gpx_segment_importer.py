b16864b520009934bd65bd146e8a3da297d311e6
SGroe <SGroechenig@gmx.at>
2017-12-19 10:06:07 +0100

    New SVG icon
    PNG icon replaced with SVG icon

--- gpx_segment_importer.py
+++ gpx_segment_importer.py
@@ -178,7 +178,7 @@ class GpxSegmentImporter:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/GpxSegmentImporter/icon.png'
+        icon_path = ':/plugins/GpxSegmentImporter/icon.svg'
         self.add_action(
             icon_path,
             text=self.tr(u'GPX Segment Importer'),
